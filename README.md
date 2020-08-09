f3e1227f1057ada2ed32fbde2babe2aa999af207
Gaurav Choudhary <gaurav4741@gmail.com>
2020-08-10 00:11:43 +0530

    Added README.md file

---
+++ README.md
@@ -0,0 +1,38 @@
+# How to setup
+
+Copy the parkingLot folder.
+Setup Node Environment on your system.
+Cd.. inside the parkingLot directory / folder and run `npm i` to install all the dependencies.
+Run `npm link` to use the source file as executable
+
+# How to write commands
+
+Create a text file with whatever name you desired
+Add the instructions / commands in it as shown below. You can also copy the same.
+
+create_parking_lot 6
+park KA-01-HH-1234
+park KA-01-HH-9999
+park KA-01-BB-0001
+park KA-01-HH-7777
+park KA-01-HH-2701
+park KA-01-HH-3141
+park KA-01-HH-5621
+park KA-01-HH-2131
+leave KA-01-HH-3141 4
+status
+park KA-01-P-333
+park DL-12-AA-9999
+leave KA-01-HH-1234 4
+leave KA-01-BB-0001 6
+leave DL-12-AA-9999 2
+park KA-09-HH-0987
+park CA-09-IO-1111
+park KA-09-HH-0123
+status
+
+# How to run
+
+Go to the terminal
+Cd.. to the parkingLot directory / folder
+Type / run commmand : `parkingLot <textfilename.txt>` textfilename is the name of the text file created above
--- index.js
+++ index.js
@@ -89,83 +89,82 @@
 
 // Parking Slot
 class ParkingLot {
-  constructor(size) {
-    this.size = parseInt(size);
-    this.parkingArea = Array(6).fill(null);
-    this.totalCars = 0;
-    console.log(
-      "\x1b[32m%s\x1b[0m",
-      `Created parking lot with ${this.size} slots`
-    );
-  }
-
-  parkCar(registrationNumber) {
-    if (this.isVacant()) {
-      // Check the nearest vacant place in the Parking Area
-      for (let i = 0; i < this.size; i++) {
-        if (this.parkingArea[i] === null) {
-          this.parkingArea[i] = registrationNumber;
-          this.totalCars++;
-          console.log(`Allocated slot number: ${i + 1}`);
-          break;
-        }
-      }
-    } else {
-      console.log("\x1b[31m%s\x1b[0m", "Sorry, Parking lot is full");
-      return "Sorry, Parking lot is full";
-    }
-  }
-
-  leaveCar(registrationNumber, duration) {
-    // Stores whether car is parked or not
-    let isParked = false;
-    for (let i = 0; i < this.size; i++) {
-      if (this.parkingArea[i] === registrationNumber) {
-        this.parkingArea[i] = null;
-        this.totalCars--;
-        isParked = true;
+    constructor(size) {
+        this.size = parseInt(size);
+        this.parkingArea = Array(6).fill(null);
+        this.totalCars = 0;
         console.log(
-          "\x1b[33m%s\x1b[0m",
-          `Registration number ${registrationNumber} with slot number ${
+            "\x1b[32m%s\x1b[0m",
+            `Created parking lot with ${this.size} slots`
+        );
+    }
+
+    parkCar(registrationNumber) {
+        if (this.isVacant()) {
+            // Check the nearest vacant place in the Parking Area
+            for (let i = 0; i < this.size; i++) {
+                if (this.parkingArea[i] === null) {
+                    this.parkingArea[i] = registrationNumber;
+                    this.totalCars++;
+                    console.log(`Allocated slot number: ${i + 1}`);
+                    break;
+                }
+            }
+        } else {
+            console.log("\x1b[31m%s\x1b[0m", "Sorry, Parking lot is full");
+            return "Sorry, Parking lot is full";
+        }
+    }
+
+    leaveCar(registrationNumber, duration) {
+        // Stores whether car is parked or not
+        let isParked = false;
+        for (let i = 0; i < this.size; i++) {
+            if (this.parkingArea[i] === registrationNumber) {
+                this.parkingArea[i] = null;
+                this.totalCars--;
+                isParked = true;
+                console.log(
+                    "\x1b[33m%s\x1b[0m",
+                    `Registration number ${registrationNumber} with slot number ${
             i + 1
           } is free with Charge $${this.parkingCharge(parseInt(duration))}`
-        );
-      }
+                );
+            }
+        }
+        // If car is not parked
+        if (!isParked) {
+            console.log("\x1b[31m%s\x1b[0m", "Car is not in the parking lot");
+        }
     }
-    // If car is not parked
-    if (!isParked) {
-      console.log("\x1b[31m%s\x1b[0m", "Car is not in the parking lot");
-    }
-  }
 
-  parkingCharge(duration) {
-    if (duration > 2) {
-      return (duration - 2) * 10 + 10;
-    } else {
-      return 10;
+    parkingCharge(duration) {
+        if (duration > 2) {
+            return (duration - 2) * 10 + 10;
+        } else {
+            return 10;
+        }
     }
-  }
 
-  status() {
-    const parkedCars = [];
-    for (let i = 0; i < this.size; i++) {
-      parkedCars.push({
-        "Slot No.": i + 1,
-        "Registration No.": this.parkingArea[i]
-          ? this.parkingArea[i]
-          : "Slot Empty...",
-      });
+    status() {
+        const parkedCars = [];
+        for (let i = 0; i < this.size; i++) {
+            parkedCars.push({
+                "Slot No.": i + 1,
+                "Registration No.": this.parkingArea[i] ?
+                    this.parkingArea[i] : "Slot Empty...",
+            });
+        }
+        console.table(parkedCars);
     }
-    console.table(parkedCars);
-  }
 
-  isVacant() {
-    if (this.size === this.totalCars) {
-      return false;
-    } else {
-      return true;
+    isVacant() {
+        if (this.size === this.totalCars) {
+            return false;
+        } else {
+            return true;
+        }
     }
-  }
 }
 
 module.exports = ParkingLot;
